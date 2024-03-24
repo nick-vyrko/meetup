1b4afdcfbea1dea79da9f105e60b2ddac4a9ae74
Nikita Vyrko <nikita.vyrko@gmail.com>
2024-03-25 01:33:11 +0300

    Create sessions controller, views, routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user
+
+  private
+
+  attr_reader :current_user
+
+  def authenticate_user
+    return redirect_to root_path if session[:user_id].blank?
+
+    user = User.find_by(id: session[:user_id])
+
+    if user.blank?
+      session.delete(:user_id)
+      return redirect_to root_path
+    end
+
+    @current_user = user
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  skip_before_action :authenticate_user
+
+  def new
+    return redirect_to '' if session[:user_id].present?
+
+    @user = User.new
+  end
+
+  def create
+    if user && user.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to '' #events_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+  end
+
+  private
+
+  def user
+    return @user if defined?(@user)
+
+    @user = User.find_by(email: session_params[:email])
+  end
+
+  def session_params
+    params.require(:user).permit(:email, :password)
+  end
+end
